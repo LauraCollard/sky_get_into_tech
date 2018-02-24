a077c91d5e9ac7ee95ee799837a86086eb39f6fe
Laura Collard <35954013+LauraCollard@users.noreply.github.com>
2018-02-24 18:06:01 +0000

    Add exercise8

---
+++ Exercise8.php
@@ -0,0 +1,35 @@
+<?php
+
+echo "Please enter the operation:\n";
+$operation = stream_get_line(STDIN, 100, "\n");
+echo "Enter the first number:\n";
+$numOne = stream_get_line(STDIN, 100, "\n");
+echo "Enter the second number:\n";
+$numTwo = stream_get_line(STDIN, 100, "\n");
+
+switch($operation){
+    case "+":
+        echo "Your result is: ", $numOne + $numTwo, ".";
+        break;
+    
+    case "-":
+        echo "Your result is: ", $numOne - $numTwo, ".";
+        break;
+    
+    case "*":
+        echo "Your result is: ", $numOne * $numTwo, ".";
+        break;
+    
+    case "/":
+        if ($numTwo==0){
+            die("You can't divide by zero!");
+            break;
+        }
+        echo "Your result is: ", $numOne / $numTwo, ".";
+        break;
+        
+    default:
+        die("Wrong operation!\n");
+}
+
+?>
